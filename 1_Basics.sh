62a831b5d6d4b88bddd303e5e36c13563e0c08ae
satwash <134083826+satwash@users.noreply.github.com>
2024-03-01 11:49:16 +0530

    update

--- 1_Basics.sh
+++ 1_Basics.sh
@@ -119,7 +119,24 @@ echo "COLOR: $COLOR"
 done
 
 :'******************************************************************************************'
+$0 	        - The name of the Bash script.
+$1	        - $9 - The first 9 arguments to the Bash script. (As mentioned above.)
+$#  	    - How many arguments were passed to the Bash script.
+$@ 	        - All the arguments supplied to the Bash script.(all arg as sep entry)
+$*	        - All arg as a single entry except $0
+$? 	        - The exit status of the most recently run process.
+$$ 	        - The process ID of the current script. pid
+$USER 	    - The username of the user running the script.
+$HOSTNAME 	- The hostname of the machine the script is running on.
+$SECONDS 	- The number of seconds since the script was started.
+$RANDOM 	- Returns a different random number each time is it referred to.
+$LINENO 	- Returns the current line number in the Bash script.
+
 :'******************************************************************************************'
+read n
+echo "Welcome $n"
+
+OP: Welcome Dan
 :'******************************************************************************************'
 :'******************************************************************************************'
 :'******************************************************************************************'
---
+++ Scripts/check_if_file_exist.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+# Check if a directory exists
+if [ -d "/path/to/directory" ]; then
+    echo "Directory exists"
+else
+    echo "Directory does not exist"
+fi
+
+
+#################################################3
+
+# Check if a file exists
+if [ -f "/path/to/file" ]; then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi
+
+
